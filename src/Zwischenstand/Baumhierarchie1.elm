e2ec19cbab8442d9091d6fe544665d3a76be8315
milu1992 <92994779+milu1992@users.noreply.github.com>
2021-11-12 14:26:38 +0100

    import

--- src/Zwischenstand/Baumhierarchie1.elm
+++ src/Zwischenstand/Baumhierarchie1.elm
@@ -2,3 +2,6 @@ module Zwischenstand.Baumhierarchie1 exposing (..)
 
 import Browser
 import Color 
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+import Http
