6f264679dcf959e53b2bace4e4e678572dc7e1f4
milu1992 <92994779+milu1992@users.noreply.github.com>
2021-11-12 14:34:18 +0100

    Import4

--- src/Zwischenstand/Baumhierarchie1.elm
+++ src/Zwischenstand/Baumhierarchie1.elm
@@ -8,3 +8,5 @@ import Http
 import TreeDiagram
 import TreeDiagram.Svg
 import TypedSvg exposing (circle, g, line, path, rect, style, svg, text_)
+import TypedSvg.Attributes exposing (class, d, fill, fontFamily, fontSize, stroke, strokeWidth, textAnchor, transform, viewBox)
+import TypedSvg.Attributes.InPx exposing (cx, cy, height, r, width, x, x1, x2, y, y1, y2)
