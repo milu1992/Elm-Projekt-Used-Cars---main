d961cfb266ed5a3596445b253001d5d847c45f6e
milu1992 <92994779+milu1992@users.noreply.github.com>
2021-11-14 14:47:34 +0100

    import packages

--- src/Zwischenstand/Baumhierarchie1.elm
+++ src/Zwischenstand/Baumhierarchie1.elm
@@ -1,14 +1,15 @@
 module Zwischenstand.Baumhierarchie1 exposing (..)
 
 import Browser
-import Color 
+import Color
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 import Http
+import Json.Decode
 import TreeDiagram
 import TreeDiagram.Svg
 import TypedSvg exposing (circle, g, line, path, rect, style, svg, text_)
 import TypedSvg.Attributes exposing (class, d, fill, fontFamily, fontSize, stroke, strokeWidth, textAnchor, transform, viewBox)
-import TypedSvg.Attributes.InPx exposing (cx, cy, height, r, width, x, x1, x2, y, y1, y2) 
+import TypedSvg.Attributes.InPx exposing (cx, cy, height, r, width, x, x1, x2, y, y1, y2)
 import TypedSvg.Core exposing (Svg)
 import TypedSvg.Types as ST exposing (AnchorAlignment(..), Length(..), Paint(..), Transform(..))
