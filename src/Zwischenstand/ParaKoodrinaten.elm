5c4baf68b920880b16f5f414de8142c78aaa3434
milu1992 <92994779+milu1992@users.noreply.github.com>
2021-11-16 18:17:33 +0100

    Anlegen Datensruktur

---
+++ src/Zwischenstand/ParaKoodrinaten.elm
@@ -0,0 +1,21 @@
+module Zwischenstand.ParaKoodrinaten exposing (..)
+
+import Axis
+import Browser
+import Color
+import Csv
+import Csv.Decode
+import Html exposing (Html, a, li, ul)
+import Html.Attributes exposing (href)
+import Html.Events exposing (onClick)
+import Http
+import List.Extra
+import Path
+import Scale exposing (ContinuousScale)
+import Shape
+import Statistics
+import TypedSvg exposing (circle, g, line, path, rect, style, svg, text_)
+import TypedSvg.Attributes exposing (class, d, fill, fontFamily, fontSize, stroke, strokeWidth, textAnchor, transform, viewBox)
+import TypedSvg.Attributes.InPx exposing (cx, cy, height, r, width, x, x1, x2, y, y1, y2)
+import TypedSvg.Core exposing (Svg)
+import TypedSvg.Types exposing (AnchorAlignment(..), Length(..), Paint(..), Transform(..))
