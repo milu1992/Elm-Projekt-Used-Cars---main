c985394e386ead2aabf240ae160edff6a9efed0a
milu1992 <92994779+milu1992@users.noreply.github.com>
2021-11-16 18:18:32 +0100

    Parallele Koordinaten Start

--- src/Zwischenstand/ParaKoodrinaten.elm
+++ src/Zwischenstand/ParaKoodrinaten.elm
@@ -19,3 +19,4 @@ import TypedSvg.Attributes exposing (class, d, fill, fontFamily, fontSize, strok
 import TypedSvg.Attributes.InPx exposing (cx, cy, height, r, width, x, x1, x2, y, y1, y2)
 import TypedSvg.Core exposing (Svg)
 import TypedSvg.Types exposing (AnchorAlignment(..), Length(..), Paint(..), Transform(..))
+
