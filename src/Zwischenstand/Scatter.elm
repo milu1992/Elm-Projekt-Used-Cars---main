29623fecd53c52798c198cfef18e6fb5986cbe6c
milu1992 <92994779+milu1992@users.noreply.github.com>
2021-11-18 12:48:32 +0100

    Scatter FIrst

---
+++ src/Zwischenstand/Scatter.elm
@@ -0,0 +1,16 @@
+module Zwischenstand.Scatter exposing (..)
+
+import Axis
+import Html exposing (Html,text, pre)
+import Http
+import Scale exposing (ContinuousScale)
+import Statistics
+import TypedSvg exposing (circle, g, rect, style, svg, text_)
+import TypedSvg.Attributes exposing (class, fontFamily, fontSize, textAnchor, transform, viewBox)
+import TypedSvg.Attributes.InPx exposing (cx, cy, height, r, width, x, y)
+import TypedSvg.Core exposing (Svg, text)
+import TypedSvg.Types exposing (AnchorAlignment(..), FontWeight(..), Length(..), Transform(..), px)
+import Csv
+import Csv.Decode
+import Browser
+
