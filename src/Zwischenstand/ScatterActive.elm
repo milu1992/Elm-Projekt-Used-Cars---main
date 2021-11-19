2e50895bf9c6d597af9859b743b9f713168dcbf7
milu1992 <92994779+milu1992@users.noreply.github.com>
2021-11-19 14:01:31 +0100

    Import

---
+++ src/Zwischenstand/ScatterActive.elm
@@ -0,0 +1,19 @@
+module Zwischenstand.ScatterActive exposing (..)
+
+import Axis
+import Html exposing (Html,text, pre)
+import Http
+import Scale exposing (ContinuousScale)
+import Statistics
+import TypedSvg exposing (circle, g, rect, style, svg, text_)
+import TypedSvg.Attributes exposing (class, fontFamily, fontSize, textAnchor, transform, viewBox)
+import TypedSvg.Attributes.InPx exposing (cx, cy, height, r, width, x, y)
+import TypedSvg.Core exposing (Svg, text)
+import TypedSvg.Types exposing (AnchorAlignment(..), FontWeight(..), Length(..), Transform(..), px)
+import Csv
+import Csv.Decode
+import Browser
+import TypedSvg.Attributes exposing (name)
+import Html exposing (ul)
+import Html exposing (li)
+import Html.Events exposing (onClick)
