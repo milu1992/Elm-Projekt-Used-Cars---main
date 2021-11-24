cfbb094ef7642f54005b794cfc29f4f14eee209b
milu1992 <92994779+milu1992@users.noreply.github.com>
2021-11-24 16:32:50 +0100

    Data error

--- Final/scatterPlotActiveFinal.elm
+++ Final/scatterPlotActiveFinal.elm
@@ -60,7 +60,7 @@ getData x =
         |> List.map
             (\datensatz ->
                 Http.get
-                    { url = "https://raw.githubusercontent.com/RicBre/Elm-Projekt-WineInformation/main/Daten/AufbereiteteDaten/" ++ datensatz
+                    { url = "https://github.com/milu1992/Elm-Projekt-Used-Cars---main/blob/3e9cb1501939e0698ad93d5202ce759db2661c28/Data/Quelldaten/" ++ datensatz
                     , expect = Http.expectString x
                     }
             )
@@ -68,7 +68,7 @@ getData x =
 
 liste : List String
 liste =
-    [ "CarCleanFinal.csv.csv"]
+    [ "Final.csv"]
 
 csvString_to_data : String -> List Cars
 csvString_to_data csvRaw =
