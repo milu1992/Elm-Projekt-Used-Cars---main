564cd90a46287708e047f44f8b6d1bc6502f9343
milu1992 <92994779+milu1992@users.noreply.github.com>
2021-11-26 11:47:14 +0100

    color

--- Final/scatterPlotActiveFinal.elm
+++ Final/scatterPlotActiveFinal.elm
@@ -230,7 +230,7 @@ scatterplot model =
         [ style [] [ TypedSvg.Core.text """
             .point circle { stroke: rgba(0, 0, 0,0.4); fill: rgba(255, 255, 255,0.3); }
             .point text { display: none; }
-            .point:hover circle { stroke: rgba(0, 0, 0,1.0); fill: rgb(118, 214, 78); }
+            .point:hover circle { stroke: rgba(0, 0, 0,1.0); fill: rgb(230, 0, 38); }
             .point:hover text { display: inline; }
           """ ]
         , g [ transform [ Translate 60 390 ] ]
@@ -239,7 +239,7 @@ scatterplot model =
                 [ x (Scale.convert xScaleLocal labelPositions.x)
                 , y 35
 
-                , fontFamily [ "Helvetica", "sans-serif" ]
+                , fontFamily [ "calibri", "sans-serif" ]
                 , fontSize (px 20)
 
             
@@ -252,7 +252,7 @@ scatterplot model =
                 [ x -30
                 , y -30
 
-                , fontFamily [ "Helvetica", "sans-serif" ]
+                , fontFamily [ "calibri", "sans-serif" ]
                 , fontSize (px 20)
 
               
